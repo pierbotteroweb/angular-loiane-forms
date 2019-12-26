1444cb2d2dac4d265ad113d86cf594684880aef1
pierbotteroweb <pierbotteroweb@gmail.com>
2019-12-26 09:55:43 -0300

    Aula 78 - Forms. CSS para validação de campos

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'Loiane',
-    email: 'loiane@email.com',
+    nome: null,
+    email: null,
     cpf: ''
   }
 
