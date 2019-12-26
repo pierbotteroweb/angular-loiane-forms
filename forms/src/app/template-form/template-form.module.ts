42605b5aba3c93a31124b26cc9336deb92cc5f74
pierbotteroweb <pierbotteroweb@gmail.com>
2019-12-26 13:52:47 -0300

    Aulas 80 e 81 Forms. Bloqueando submit para form invalid e fazendo display de dados do formulario renderizados em tela

--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -1,3 +1,4 @@
+import { FormDebugComponent } from './../form-debug/form-debug.component';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,7 +6,8 @@ import { TemplateFormComponent } from './template-form.component';
 
 @NgModule({
   declarations: [
-    TemplateFormComponent
+    TemplateFormComponent,
+    FormDebugComponent
   ],
   imports: [
     CommonModule,
