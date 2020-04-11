27d7bfc1f745613a5d2563b36b242fab5b16a6bd
Pier Branco <pier.branco@bit.com.br>
2020-04-11 01:30:10 -0300

    Aula 112 - Configuração de serviço de mensagem de erro dinamico, sem precisar ficar repetindo o componente pra cada tipo de erro necessário

--- forms/src/app/shared/form-validations.ts
+++ forms/src/app/shared/form-validations.ts
@@ -62,4 +62,15 @@ export class FormValidations{
 
       return validator
     }
+
+    static getErrorMsg(fieldName: string, validatorName: string, validatorValue?: any){
+      const config = {
+        'required': `${fieldName} é obrigatório.`,
+        'minlength': `${fieldName} precisa ter no mínimo ${validatorValue.requiredLength} caracteres`,
+        'maxlength': `${fieldName} precisa ter no maximo ${validatorValue.requiredLength} caracteres`,
+        'cepInvalido': "CEP inválido"
+      }
+
+      return config[validatorName]
+    }
 }
--- forms/src/app/shared/shared/shared.module.ts
+++ forms/src/app/shared/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormDebugComponent } from '../form-debug/form-debug.component';
 import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
+import { ErrorMsgComponent } from '../error-msg/error-msg.component';
 
 @NgModule({
   declarations: [
     FormDebugComponent,
-    CampoControlErroComponent
+    CampoControlErroComponent,
+    ErrorMsgComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     FormDebugComponent,
-    CampoControlErroComponent
+    CampoControlErroComponent,
+    ErrorMsgComponent
   ]
 })
 export class SharedModule { }
